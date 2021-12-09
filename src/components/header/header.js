8674783e6ed3907b3da598e1f2d3f646571164c2
HopeKayumov <umidjonkayumov2000@gmail.com>
2021-12-10 01:12:03 +0500

    add auth with firebase

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,85 +1,113 @@
-import React, {useState} from 'react';
-import './header.css'
-import logo from './img/udevs-logo.svg'
-import headerNotification from './img/notifications.svg'
-import {Link} from "react-router-dom";
-import User from './img/user.png'
-import Cancel from './img/cancel.png'
+import React, { useState, useRef } from "react";
+import "./header.css";
+import logo from "./img/udevs-logo.svg";
+import headerNotification from "./img/notifications.svg";
+import { Link } from "react-router-dom";
+import User from "./img/user.png";
+import Cancel from "./img/cancel.png";
+import { signUp, logOut, useAuth } from "../../firebase-config";
 
 export default function Headers() {
-    const [isOpen, setIsOpen] = useState(false)
-    const [profileOpen, setProfileOpen] = useState(false)
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const isAuthorized = localStorage.getItem("isAuthorized")
+  const [isOpen, setIsOpen] = useState(false);
+  const [profileOpen, setProfileOpen] = useState(false);
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const [loading, setLoading] = useState(false);
+  const currentUser = useAuth();
 
-    function onSubmit (event) {
-        event.preventDefault()
-        const admin = {
-            email: 'umidjon@gmail.com',
-            password: '12345'
-        }
-        if (admin.email === email && admin.password === password) {
-            localStorage.setItem("isAuthorized", true)
-        }
-        else {
-            alert('Email or Password incorrect!')
-        }
+  async function handleSignUp() {
+    try {
+      setLoading(true);
+      await signUp(emailRef.current.value, passwordRef.current.value);
+    } catch {
+      alert("error");
     }
-    function logOut () {
-        localStorage.removeItem("isAuthorized")
+    setLoading(false);
+  }
+  async function handleLogOut() {
+    setLoading(true);
+    try {
+      await logOut();
+    } catch {
+      alert("error");
     }
+    setLoading(false);
+    window.location.reload();
+  }
 
-    return (
-        <div className='header'>
-           <Link to="/">
-               <img src={logo} className='header-logo' alt="logo"/>
-           </Link>
+  return (
+    <div className="header">
+      <Link to="/">
+        <img src={logo} className="header-logo" alt="logo" />
+      </Link>
 
-            <div className='header-reg-panel'>
-                <button className='header-notification-btn'>
-                    <img src={headerNotification} className='header-notification' alt="notification"/>
-                </button>
-                <span className='header-notification-count'>1</span>
-                {!isAuthorized && (
-                    <button className='header-login' onClick={() => setIsOpen(true)}>Войти</button>
-                )}
-                <div className="user-profile">
-                    {isAuthorized && (
-                        <button className="user-profile__btn" onClick={() => setProfileOpen(true)}>
-                            <img className="user-profile__img" src={User} alt="user profile"/>
-                        </button>
-                    )}
-                    <div className={`user-profile__dash ${profileOpen ? 'visible' : ''}`}>
-                        <button className="user-profile__out" onClick={logOut}>Log out</button>
-                    </div>
-                </div>
-                 {/*Modal */}
-                <div className={`modal ${isOpen ? 'visible' : ''}`}>
-                    <button className="modal-close" onClick={() => setIsOpen(false)}>
-                        <img src={Cancel} alt="cancel svg" width="22px" height="22px"/>
-                    </button>
-                    <h3 className="modal-title">Вход на udevs news</h3>
-
-                    <form className="modal-form" onSubmit={onSubmit}>
-                        <input
-                            className="modal-email"
-                            type="email" name="email"
-                            placeholder="Email"
-                            onChange={event => setEmail(event.target.value)}
-                        />
-                        <input
-                            className="modal-password"
-                            type="password"
-                            name="password"
-                            placeholder="Password"
-                            onChange={event => setPassword(event.target.value)}
-                        />
-                        <button className="modal-submit" type="submit">Войти</button>
-                    </form>
-                </div>
-                {isOpen && <div className="overlay"></div>}
-            </div>
+      <div className="header-reg-panel">
+        <button className="header-notification-btn">
+          <img
+            src={headerNotification}
+            className="header-notification"
+            alt="notification"
+          />
+        </button>
+        <span className="header-notification-count">1</span>
+        {!currentUser && (
+          <button className="header-login" onClick={() => setIsOpen(true)}>
+            Войти
+          </button>
+        )}
+        <div className="user-profile">
+          {currentUser && (
+            <button
+              className="user-profile__btn"
+              onClick={() => setProfileOpen(true)}
+            >
+              <img
+                className="user-profile__img"
+                src={User}
+                alt="user profile"
+              />
+            </button>
+          )}
+          <div className={`user-profile__dash ${profileOpen ? "visible" : ""}`}>
+            <button className="user-profile__out" onClick={handleLogOut}>
+              Log out
+            </button>
+          </div>
         </div>
-    );
+        {/*Modal */}
+        <div className={`modal ${isOpen ? "visible" : ""}`}>
+          <button className="modal-close" onClick={() => setIsOpen(false)}>
+            <img src={Cancel} alt="cancel svg" width="22px" height="22px" />
+          </button>
+          <h3 className="modal-title">Вход на udevs news</h3>
+
+          <form className="modal-form" onSubmit={signUp}>
+            <input
+              className="modal-email"
+              type="email"
+              name="email"
+              placeholder="Email"
+              ref={emailRef}
+            />
+            <input
+              className="modal-password"
+              type="password"
+              name="password"
+              placeholder="Password"
+              ref={passwordRef}
+            />
+            <button
+              disabled={loading}
+              className="modal-submit"
+              type="submit"
+              onClick={handleSignUp}
+            >
+              Войти
+            </button>
+          </form>
+        </div>
+        {isOpen && <div className="overlay"></div>}
+      </div>
+    </div>
+  );
 }
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth'
+import { useState, useEffect } from "react";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCptc2scSfaJm3Hzp1ycqZLAdQOPpc2o94",
@@ -12,3 +14,19 @@ const firebaseConfig = {
 };
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
+const auth = getAuth();
+
+export function signUp(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password)
+}
+export function logOut() {
+    return signOut(auth)
+}
+export function useAuth() {
+    const [currentUser, setCurrentUser] = useState();
+    useEffect(() => {
+       const unsub =  onAuthStateChanged(auth, user => setCurrentUser(user));
+       return unsub;
+    }, [])
+    return currentUser;
+}
